1ae745ded57e915d101e2a6395b03ee1c9b7f5da
Thales Eduardo <thalesdev22@gmail.com>
2021-10-07 09:52:04 -0300

    alt useCaseCreatePost.

--- src/modules/post/useCases/createPost/CreatePostUseCase.ts
+++ src/modules/post/useCases/createPost/CreatePostUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { AppErrors } from '../../../../shared/errors/AppErrors';
 
 import { Post } from '../../entities/Post';
 
@@ -24,6 +25,17 @@ export class CreatePostUseCase {
     immediate,
     urgent,
   }: IRequest): Promise<Post> {
+    if (check === true && immediate === true && urgent === true) {
+      throw new AppErrors('Apenas uma das prioridade pode ser true');
+    }
+    if (
+      (immediate === true && check === true) ||
+      (urgent === true && check === true) ||
+      (urgent === true && immediate === true)
+    ) {
+      throw new AppErrors('Apenas uma das prioridade pode ser true');
+    }
+
     const product = await this.postRepository.create({
       title,
       check,
